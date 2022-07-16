3f601fa13dabbd0ef61a84e073acfe3bf0946df6
Daigo Hirooka <daigo.hirooka@gmail.com>
2022-07-16 23:55:14 +0900

    add model and train scripts

---
+++ model.py
@@ -0,0 +1,220 @@
+from dataclasses import field
+from typing import List, Tuple, Union
+
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+
+
+def sinusoidal_embedding(x,
+                         min_freq: float = 1.0,
+                         max_freq: float = 1000.0,
+                         embedding_dims: int = 32):
+    frequencies = jnp.exp(jnp.linspace(jnp.log(min_freq),
+                                       jnp.log(max_freq),
+                                       embedding_dims // 2))
+    # x: (batch, 1, 1, 1), angular_speeds: (embedding_dims,)
+    angular_speeds = 2.0*jnp.pi*frequencies
+    embeddings = jnp.concatenate([jnp.sin(angular_speeds*x),
+                                  jnp.cos(angular_speeds*x)], axis=3)
+    return embeddings  # (batch, 1, 1, embedding_dims)
+
+
+class ResidualBlock(nn.Module):
+    features: int
+
+    @nn.compact
+    def __call__(self, x, train: bool):
+        input_features = x.shape[3]
+        if input_features == self.features:
+            residual = x
+        else:
+            residual = nn.Conv(self.features, kernel_size=(3, 3))(x)
+
+        x = nn.BatchNorm(use_running_average=not train,
+                         use_bias=False, use_scale=False)(x)
+        x = nn.Conv(self.features, (3, 3), 1, 1)(x)
+        x = nn.swish(x)
+        x = nn.Conv(self.features, (3, 3), 1, 1)(x)
+        x += residual
+        return x
+
+
+class DownBlock(nn.Module):
+    features: int
+    blocks: int
+    
+    @nn.compact
+    def __call__(self, x, train: bool) -> Tuple:
+        skips = []
+        for _ in range(self.blocks):
+            x = ResidualBlock(self.features)(x, train=train)
+            skips.append(x)
+        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+        return x, skips
+
+
+def upsample2d(x,
+               scale: Union[int, Tuple[int, int]],
+               method: str = 'bilinear'):
+    b, h, w, c = x.shape
+    
+    if isinstance(scale, int):
+        h_out, w_out = scale*h, scale*w
+    elif len(scale) == 2:
+        h_out, w_out = scale[0]*h, scale[1]*w
+    else:
+        raise ValueError('scale argument should be either int'
+                         'or Tuple[int, int]')
+    
+    return jax.image.resize(x, shape=(b, h_out, w_out, c), method=method)
+
+
+class UpBlock(nn.Module):
+    features: int
+    blocks: int
+
+    @nn.compact
+    def __call__(self, x, skips: List, train: bool):
+        x = upsample2d(x, scale=2, method='bilinear')
+        for _ in range(self.blocks):
+            x = jnp.concatenate([x, skips.pop()], axis=-1)
+            x = ResidualBlock(self.features)(x, train=train)
+        return x
+
+
+class UNet(nn.Module):
+    feature_stages: List[int]
+    blocks: int
+    min_freq: float = 1.0
+    max_freq: float = 1000.0
+    embedding_dims: int = 32
+
+    @nn.compact
+    def __call__(self, noisy_images, noise_variances, train: bool):
+        embeddings = sinusoidal_embedding(noise_variances,
+                                          min_freq=self.min_freq,
+                                          max_freq=self.max_freq,
+                                          embedding_dims=self.embedding_dims)
+        *_, h, w, _ = noisy_images.shape
+        # (b, 1, 1, embedding_dims) -> (b, h, w, embedding_dims)
+        embeddings = upsample2d(embeddings, scale=(h, w), method='nearest')
+
+        x = nn.Conv(self.feature_stages[0], (1, 1))(noisy_images)
+        x = jnp.concatenate([x, embeddings], axis=-1)
+
+        skip_stages = []
+        for features in self.feature_stages[:-1]:
+            x, skips = DownBlock(features, self.blocks)(x, train=train)
+            skip_stages.append(skips)
+
+        for _ in range(self.blocks):
+            x = ResidualBlock(self.feature_stages[-1])(x, train=train)
+
+        for features in reversed(self.feature_stages[:-1]):
+            skips = skip_stages.pop()
+            x = UpBlock(features, self.blocks)(x, skips, train=train)
+
+        x = nn.Conv(3, (1, 1), kernel_init=nn.initializers.zeros)(x)
+        return x
+
+
+class DiffusionModel(nn.Module):
+    # UNet parameters
+    feature_stages: List[int] = field(default_factory=lambda:
+                                      [32, 64, 96, 128])
+    blocks: int = 2
+    min_freq: float = 1.0
+    max_freq: float = 1000.0
+    embedding_dims: int = 32
+
+    # Sampling (reverse diffusion) parameters
+    min_signal_rate: float = 0.02
+    max_signal_rate: float = 0.95
+
+    # image normalization parameters
+    # default values transform [0, 1] -> [-1, 1]
+    image_mean = jnp.array([0.5, 0.5, 0.5], dtype=jnp.float32)
+    image_std = jnp.array([0.5, 0.5, 0.5], dtype=jnp.float32)    
+    
+    def setup(self):
+        self.network = UNet(feature_stages=self.feature_stages,
+                            blocks=self.blocks,
+                            min_freq=self.min_freq,
+                            max_freq=self.max_freq,
+                            embedding_dims=self.embedding_dims)
+
+    def __call__(self, images, rng, train: bool):
+        rng_noises, rng_times = jax.random.split(rng)
+        
+        images = self.normalize(images)
+        noises = jax.random.normal(rng_noises, images.shape, images.dtype)
+        
+        diffusion_times = jax.random.uniform(rng_times,
+                                             (images.shape[0], 1, 1, 1),
+                                             images.dtype)
+        noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+        noisy_images = signal_rates*images + noise_rates*noises
+        pred_noises, pred_images = self.denoise(noisy_images,
+                                                noise_rates,
+                                                signal_rates,
+                                                train=train)
+        return noises, images, pred_noises, pred_images
+
+    def normalize(self, images):
+        mean = jnp.reshape(self.image_mean, (1, 1, 1, -1))
+        std = jnp.reshape(self.image_std, (1, 1, 1, -1))
+        return (images - mean)/std
+
+    def denormalize(self, images):
+        mean = jnp.reshape(self.image_mean, (1, 1, 1, -1))
+        std = jnp.reshape(self.image_std, (1, 1, 1, -1))
+        images = std*images + mean
+        return jnp.clip(images, 0.0, 1.0)
+
+    def diffusion_schedule(self, diffusion_times):
+        start_angle = jnp.arccos(self.max_signal_rate)
+        end_angle = jnp.arccos(self.min_signal_rate)
+
+        diffusion_angles = start_angle + diffusion_times*(end_angle - start_angle)
+
+        # angles -> signal and noise rates
+        signal_rates = jnp.cos(diffusion_angles)
+        noise_rates = jnp.sin(diffusion_angles)
+        return noise_rates, signal_rates
+
+    def denoise(self, noisy_images, noise_rates, signal_rates, train: bool):
+        pred_noises = self.network(noisy_images, noise_rates**2, train=train)
+        pred_images = (noisy_images - noise_rates*pred_noises) / signal_rates
+        return pred_noises, pred_images
+
+    def reverse_diffusion(self, initial_noise, diffusion_steps):
+        n_images = initial_noise.shape[0]
+        step_size = 1.0 / diffusion_steps
+
+        next_noisy_images = initial_noise
+        for step in range(diffusion_steps):
+            noisy_images = next_noisy_images
+
+            # separate noisy image into noise/image
+            ones = jnp.ones((n_images, 1, 1, 1), dtype=initial_noise.dtype)
+            diffusion_times =  ones - step * step_size
+            noise_rates, signal_rates = self.diffusion_schedule(diffusion_times)
+            pred_noises, pred_images = self.denoise(noisy_images,
+                                                    noise_rates,
+                                                    signal_rates,
+                                                    train=False)
+
+            next_diffusion_times = diffusion_times - step_size
+            next_noise_rates, next_signal_rates \
+                = self.diffusion_schedule(next_diffusion_times)
+            next_noisy_images = next_signal_rates * pred_images \
+                + next_noise_rates * pred_noises
+
+        return pred_images
+
+    def generate(self, rng, image_shape, diffusion_steps: int):
+        initial_noise = jax.random.normal(rng, image_shape)
+        generated_images = self.reverse_diffusion(initial_noise, diffusion_steps)
+        generated_images = self.denormalize(generated_images)
+        return generated_images
---
+++ train.py
@@ -0,0 +1,203 @@
+import argparse
+from tqdm import tqdm
+from pathlib import Path
+from typing import Tuple, Any
+from datetime import datetime
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+from flax import linen as nn
+from flax.training import train_state
+from flax.training.checkpoints import save_checkpoint
+import optax
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from model import DiffusionModel
+
+
+def create_output_dir(output_dir: Path) -> Tuple[Path, Path, Path]:
+    output_dir = output_dir / datetime.now().strftime('%Y%m%d-%H%M%S')
+    ckpt_dir = output_dir / 'models'
+    log_dir = output_dir / 'logs'
+    
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+        ckpt_dir.mkdir()
+        log_dir.mkdir()
+
+    return (output_dir, ckpt_dir, log_dir)
+
+
+def preprocess_image(data, image_size):
+    image = data['image']
+    height = tf.shape(image)[0]
+    width = tf.shape(image)[1]
+    crop_size = tf.minimum(height, width)
+    image = tf.image.crop_to_bounding_box(image,
+                                          (height - crop_size) // 2,
+                                          (width - crop_size) // 2,
+                                          crop_size,
+                                          crop_size)
+    # resize and clip
+    # for image downsampling it is important to turn on antialiasing
+    image = tf.image.resize(image, size=(image_size, image_size),
+                            antialias=True)
+    return tf.clip_by_value(image / 255.0, 0.0, 1.0)
+
+
+def prepare_datasets(image_size: int = 64,
+                     batch_size: int = 64):
+    dataset_name = 'oxford_flowers102'
+    split_train = 'train[:80%]+validation[:80%]+test[:80%]'
+    split_val = 'train[80%:]+validation[80%:]+test[80%:]'
+
+    preprocess_fn = partial(preprocess_image, image_size=image_size)
+    
+    ds_train = tfds.load(dataset_name, split=split_train, shuffle_files=True)\
+                   .map(preprocess_fn, num_parallel_calls=tf.data.AUTOTUNE)\
+                   .cache()\
+                   .shuffle(buffer_size=10*batch_size)\
+                   .batch(batch_size, drop_remainder=True)\
+                   .prefetch(buffer_size=tf.data.AUTOTUNE)
+    ds_train = tfds.as_numpy(ds_train)
+                   
+    ds_val = tfds.load(dataset_name, split=split_val, shuffle_files=True)\
+                 .map(preprocess_fn, num_parallel_calls=tf.data.AUTOTUNE)\
+                 .cache()\
+                 .batch(batch_size, drop_remainder=True)\
+                 .prefetch(buffer_size=tf.data.AUTOTUNE)
+    ds_val = tfds.as_numpy(ds_val)
+
+    return ds_train, ds_val
+
+
+class TrainState(train_state.TrainState):
+    batch_stats: Any
+
+
+def model(**kwargs):
+    return DiffusionModel(**kwargs)
+
+
+def l1_loss(predictions, targets):
+    return jnp.abs(predictions - targets)
+
+
+def kernel_inception_distance():
+    raise NotImplementedError()
+
+
+def update_ema(p_cur, p_new, momentum: float = 0.999):
+    return momentum*p_cur + (1-momentum)*p_new
+
+
+@jax.jit
+def train_step(state, batch, rng):
+    def loss_fn(params):
+        outputs, mutated_vars = state.apply_fn(
+            {'params': params, 'batch_stats': state.batch_stats},
+            batch, rng, train=True,
+            mutable=['batch_stats']
+        )
+        noises, images, pred_noises, pred_images = outputs
+        
+        noise_loss = l1_loss(pred_noises, noises).mean()
+        image_loss = l1_loss(pred_images, images).mean()
+        loss = noise_loss + image_loss
+        return loss, mutated_vars
+    
+    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+    (loss, mutated_vars), grads = grad_fn(state.params)
+    state = state.apply_gradients(
+        grads=grads,
+        batch_stats=mutated_vars['batch_stats'])
+    return state, loss
+        
+
+@jax.jit
+def evaluate(params,
+             batch_stats,
+             rng,
+             n_images: int,
+             image_size: Tuple[int, int],
+             diffusion_steps: int):
+    def eval_fn(model):
+        # TODO: quantitative metrics
+        image_shape = (n_images, *image_size, 3)
+        generated_images = model.generate(rng, image_shape, diffusion_steps)
+        return generated_images
+
+    variables = {'params': params, 'batch_stats': batch_stats}
+    return nn.apply(eval_fn, model())(variables)
+
+
+def run(epochs: int,
+        image_size: int,
+        batch_size: int,
+        learning_rate: float,
+        weight_decay: float,
+        val_diffusion_steps: int,
+        output_dir: Path):
+    output_dir, ckpt_dir, log_dir = create_output_dir(output_dir)
+    summary_writer = tf.summary.create_file_writer(str(log_dir))
+    
+    rng = jax.random.PRNGKey(0)
+    rng, key_init, key_diffusion = jax.random.split(rng, 3)
+
+    ds_train, _ = prepare_datasets(image_size, batch_size)
+
+    image_shape = (batch_size, image_size, image_size, 3)
+    dummy = jnp.ones(image_shape, dtype=jnp.float32)
+
+    variables = model().init(key_init, dummy, key_diffusion,
+                             train=True)
+
+    state = TrainState.create(
+        apply_fn=model().apply,
+        params=variables['params'],
+        batch_stats=variables['batch_stats'],
+        tx=optax.adamw(learning_rate, weight_decay=weight_decay)
+    )
+    ema_params = state.params.copy(add_or_replace={})
+    rng, rng_train, rng_val = jax.random.split(rng, 3)
+
+    for epoch in range(epochs):
+        losses = []
+        for images in (pbar := tqdm(ds_train, desc=f'Epoch {epoch}')):
+            rng_train, key = jax.random.split(rng_train)
+            state, loss = train_step(state, images, key)
+
+            pbar.set_postfix({'loss': f'{loss:.5f}'})
+            losses.append(loss)
+            ema_params = jax.tree_map(update_ema, ema_params, state.params)
+
+        generated_images = evaluate(ema_params,
+                                    state.batch_stats,
+                                    rng=rng_val,
+                                    n_images=batch_size,
+                                    image_size=(image_size, image_size),
+                                    diffusion_steps=val_diffusion_steps)
+
+        with summary_writer.as_default():
+            tf.summary.scalar('loss', np.mean(losses), step=epoch)
+            tf.summary.image('generated', generated_images, step=epoch,
+                             max_outputs=8)
+        save_checkpoint(ckpt_dir, state, step=epoch)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DDIM training')
+    parser.add_argument('-e', '--epochs', type=int, default=1)
+    parser.add_argument('--image-size', type=int, default=64)
+    parser.add_argument('-b', '--batch-size', type=int, default=64)
+    parser.add_argument('-lr', '--learning-rate', type=float, default=1e-3)
+    parser.add_argument('--weight-decay', type=float, default=1e-4)
+    parser.add_argument('--val-diffusion-steps', type=int, default=20)
+    parser.add_argument('-o', '--output-dir', type=Path, default='./outputs')
+
+    args = parser.parse_args()
+
+    run(**vars(args))
